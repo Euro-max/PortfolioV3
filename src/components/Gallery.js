457e1cd3be899a3eccdba15f1c892770dcb6880b
Ahmed Wael <83090095+Euro-max@users.noreply.github.com>
2025-01-23 02:43:25 -0800

    Update Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/swiper-bundle.css'
 import { Navigation, Pagination,Scrollbar, A11y } from 'swiper/modules';
 import far from './G13.jpeg';
-import EECE from './EECE.jpeg
+import EECE from './EECE.jpeg';
 import G11 from './G11.jpeg'
 import G12 from './G12.jpeg'
 import 'swiper/css/navigation';
